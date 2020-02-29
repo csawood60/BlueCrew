8c29755de92d92589e70a34135c7c8cab4f01221
csawood60 <39203704+csawood60@users.noreply.github.com>
2020-02-28 19:15:52 -0700

    Add files via upload

---
+++ assets/include/nav.php
@@ -0,0 +1,21 @@
+<!-- 
+    This is the nav to be used for all pages.
+    All pages must be php pages
+    Use the <php include('./asset/include/nav.php'); ?> command
+ -->
+
+    <nav class="navbar navbar-dark navbar-expand-lg" style="background-color: black">
+        <div class="container"><a class="navbar-brand logo" href="#">GETBLUEIVY</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse"
+                id="navbarNav">
+                <ul class="nav navbar-nav ml-auto text-uppercase">
+                    <li class="nav-item " role="presentation"><a class="nav-link active" href="index2.php">Home</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="./strategy.php">Strategies</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://getblueivy.com/about.html" target="_blank">About Us</a></li>                  
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://getblueivy.com/events.html" target="_blank">Events</a></li> 
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://getblueivy.com/ideasforyou.html" target="_blank">Blog</a></li> 
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="https://getblueivy.com/contactblueivynow.html" target="_blank">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
---
+++ strategy.php
@@ -0,0 +1,4 @@
+<?php include('./assets/include/head.php');?>
+<?php include('./assets/include/nav.php');?> 
+<img src="./assets/img/blueIvyStrategiesPg.jpg" alt="Trulli" width="max"></div>
+<?php include('./assets/include/foot.php');?>
