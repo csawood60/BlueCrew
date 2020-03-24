2b7e7489b1bd859c9d1b85498eb4bc9cd89eb5aa
csawood60 <csawood@gmail.com>
2020-03-24 00:08:10 -0600

    Update foot.php

--- assets/include/foot.php
+++ assets/include/foot.php
@@ -3,29 +3,31 @@
     All pages must be php pages
     Use the <php include('./asset/include/Foot.php'); ?> command
  -->
-    <footer class="page-footer" style="background: black">
-        <div class="container">
-            <div class="links">
-                <a href="about.html" style="color: #34CCE3;">About</a>
-                <a href="contact.html" style="color: #34CCE3;">Contact</a>
-                <a href="events.html" style="color: #34CCE3;">Events</a>
-            </div>
-            <div class="social-icons">
-                <a href="mailto:tim@getblueivy.com"><i class="icon ion-email"></i></a>
-                <a href="https://www.facebook.com/getblueivy/"><i class="icon ion-social-facebook"></i></a>
-                
-                <a href="https://www.linkedin.com/company/getblueivy"><i class="icon ion-social-linkedin"></i></a>
-            </div>
-            <div class="links">
-                <a href="#!" style="color: #34CCE3; font-size: small;">Copywrite Blue Ivie, 2020. All rights Reserved</a>
+    <div>
+        <footer class="page-footer" style="background: black">
+            <div class="container">
+                <div class="links">
+                    <a href="about.html" style="color: #34CCE3;">About</a>
+                    <a href="contact.html" style="color: #34CCE3;">Contact</a>
+                    <a href="events.html" style="color: #34CCE3;">Events</a>
+                </div>
+                <div class="social-icons">
+                    <a href="mailto:tim@getblueivy.com"><i class="icon ion-email"></i></a>
+                    <a href="https://www.facebook.com/getblueivy/"><i class="icon ion-social-facebook"></i></a>
+                    
+                    <a href="https://www.linkedin.com/company/getblueivy"><i class="icon ion-social-linkedin"></i></a>
+                </div>
+                <div class="links">
+                    <a href="#!" style="color: #34CCE3; font-size: small;">Copyright Blue Ivie, 2020. All rights Reserved</a>
 
-            </div>            
-            <!-- <div class="links"
-                style="color: #34CCE3; font-size: small; position: relative; bottom: 0px; right: 5px;">Powered by: GetBlueIvy Team
-            </div> -->
+                </div>            
+                <!-- <div class="links"
+                    style="color: #34CCE3; font-size: small; position: relative; bottom: 0px; right: 5px;">Powered by: GetBlueIvy Team
+                </div> -->
 
-        </div>
-    </footer>
+            </div>
+        </footer>
+    </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
