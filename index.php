a4fc4418f6e3c67046c45b9623b93dac0ef864ab
csawood60 <csawood@gmail.com>
2020-03-24 23:23:08 -0600

    Update index.php
    added back in breaks

--- index.php
+++ index.php
@@ -131,7 +131,7 @@
                                             driving a positive culture, your
                                             culture will drive your organization
                                             in unintentional and undesirable
-                                            ways.</p>
+                                            ways.</p><br><br><br><br>
                                             <div class="d-flex flex-row justify-content-center">
                                                 <div class="p-4">
                                                     <a href="strategies.php#cultureArea" class="btn btn-primary">Learn More</a>
@@ -154,7 +154,7 @@
                                             needs to happen and when they are
                                             connected with a greater vision
                                             because of effective communication
-                                            processes.</p>
+                                            processes.</p><br><br><br>
                                             <div class="d-flex flex-row justify-content-center">
                                                 <div class="p-4">
                                                     <a href="strategies.php#communicationArea" class="btn btn-primary">Learn More</a>
@@ -177,7 +177,7 @@
                                             employee development will see a
                                             substantial return on investment
                                             through increased productivity,
-                                            loyalty, and retention.</p>
+                                            loyalty, and retention.</p><br>
                                             <div class="d-flex flex-row justify-content-center">
                                                 <div class="p-4">
                                                     <a href="strategies.php#developmentArea" class="btn btn-primary">Learn More</a>
