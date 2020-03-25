3690c0b8364552b27aed07d81887cb1ffcf8ffc4
csawood60 <csawood@gmail.com>
2020-03-24 23:35:06 -0600

    Update foot.php
    _blank - fix and build on footer

--- assets/include/foot.php
+++ assets/include/foot.php
@@ -12,10 +12,10 @@
                     <a href="events.html" style="color: #34CCE3;">Events</a>
                 </div>
                 <div class="social-icons">
-                    <a href="mailto:tim@getblueivy.com"><i class="icon ion-email"></i></a>
-                    <a href="https://www.facebook.com/getblueivy/"><i class="icon ion-social-facebook"></i></a>
+                    <a href="mailto:tim@getblueivy.com" target="_blank"><i class="icon ion-email"></i></a>
+                    <a href="https://www.facebook.com/getblueivy/" target="_blank"><i class="icon ion-social-facebook"></i></a>
                     
-                    <a href="https://www.linkedin.com/company/getblueivy"><i class="icon ion-social-linkedin"></i></a>
+                    <a href="https://www.linkedin.com/company/getblueivy" target="_blank"><i class="icon ion-social-linkedin"></i></a>
                 </div>
                 <div class="links">
                     <a href="#!" style="color: #34CCE3; font-size: small;">Copyright Blue Ivy Professional Services, LLC., 2020. All rights Reserved</a>
@@ -28,9 +28,9 @@
             </div>
         </footer>
     </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" target="_blank" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" target="_blank" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" target="_blank" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"  integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"  integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"  integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
     <!-- Start of HubSpot Embed Code -->
     <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/6110584.js"></script>
